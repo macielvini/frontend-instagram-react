a992d1858579b47ae7c73bfe5f0c8df674d4d882
VInicius Maciel <vm.viniciusmaciel@gmail.com>
2022-09-15 22:55:17 -0300

    Post são mostrados de forma dinamica com o map

--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,10 @@
-export default function Post() {
+export default function Post(props = "") {
   return (
     <div class="post">
       <div class="topo">
         <div class="usuario">
-          <img src="assets/img/meowed.svg" />
-          meowed
+          <img src={props.userImagem} alt={props.alt} />
+          {props.userNome}
         </div>
         <div class="acoes">
           <ion-icon name="ellipsis-horizontal"></ion-icon>
@@ -12,7 +12,7 @@ export default function Post() {
       </div>
 
       <div class="conteudo">
-        <img src="assets/img/gato-telefone.svg" />
+        <img src={props.postImagem} alt={props.alt} />
       </div>
 
       <div class="fundo">
@@ -28,9 +28,9 @@ export default function Post() {
         </div>
 
         <div class="curtidas">
-          <img src="assets/img/respondeai.svg" />
+          <img src={props.imagemCurtidor} alt={props.alt} />
           <div class="texto">
-            Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+            Curtido por <strong>{props.nomeCurtidor}</strong> e <strong>outras {props.quantidadeCurtidas} pessoas</strong>
           </div>
         </div>
       </div>
--- src/Posts.js
+++ src/Posts.js
@@ -1,11 +1,23 @@
 import Post from "./Post";
 
+const posts = [
+  { userImagem: "assets/img/meowed.svg", userNome: "meowed", postImagem: "assets/img/gato-telefone.svg", imagemCurtidor: "assets/img/respondeai.svg", nomeCurtidor: "respondeai", quantidadeCurtidas: 101.523 },
+  { userImagem: "assets/img/barked.svg", userNome: "barked", postImagem: "assets/img/dog.svg", imagemCurtidor: "assets/img/adorable_animals.svg", nomeCurtidor: "adorable_animals", quantidadeCurtidas: 99.159 },
+];
+
 export default function Posts() {
   return (
     <div class="posts">
-      <Post />
-      <Post />
-      <Post />
+      {
+        posts.map((p) => <Post
+          userImagem={p.userImagem}
+          userNome={p.userNome}
+          postImagem={p.postImagem}
+          imagemCurtidor={p.imagemCurtidor}
+          nomeCurtidor={p.nomeCurtidor}
+          quantidadeCurtidas={p.quantidadeCurtidas} />)
+      }
+
     </div>
   )
 }
