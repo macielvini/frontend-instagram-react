d027f3facacaae439198da2bbae7e6d2c1828588
VInicius Maciel <vm.viniciusmaciel@gmail.com>
2022-09-19 13:53:29 -0300

    É possivel editar o nome, nome de usuario e foto do perfil

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,11 +1,33 @@
-export default function Usuario() {
+import React from "react"
+
+export default function Usuario(props = "") {
+
+  const [nome, setNome] = React.useState("editar");
+  const [nickname, setNickname] = React.useState("editar");
+  const [image, setImage] = React.useState("https://static.vecteezy.com/system/resources/thumbnails/005/544/770/small/profile-icon-design-free-vector.jpg")
+
+  function editarPerfil() {
+    const pNome = prompt("Qual seu nome?");
+    const pNickname = prompt("Qual seu nome de usuário?");
+    const pImage = prompt("Link para sua foto de perfil:");
+
+    setNome(pNome);
+    setNickname(pNickname);
+    setImage(pImage);
+
+  }
+
   return (
     <div class="usuario">
-      <img src="assets/img/catanacomics.svg" alt="" />
+      <img src={image} alt="" />
       <div class="texto">
-        <strong>catanacomics</strong>
-        Catana
-        <ion-icon name="pencil"></ion-icon>
+        <div>
+          <strong>{nickname}</strong>
+          {nome}
+        </div>
+        <div class="pencil-icon">
+          <ion-icon onClick={editarPerfil} name="pencil"></ion-icon>
+        </div>
       </div>
     </div>
   )
